6e07b0cce9438f70bada2a1c1249a53c7d8abe29
CherePapa <pen.aaa@mail.ru>
2025-04-24 23:38:58 +0400

    Telegram bot

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "archiver/cmd"
+import (
+	"flag"
+	"log"
+	"telegram-bot/clients/telegram"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
+)
 
 func main() {
-	cmd.Execute()
+	tgClient := telegram.New(mustToken(), tgBotHost)
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to tg-bot",
+	)
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not implaned")
+	}
+
+	return *token
 }
