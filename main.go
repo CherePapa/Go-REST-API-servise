3f4ada924e9b2e32d9748add249f2bccbf035eb2
CherePapa <pen.aaa@mail.ru>
2025-04-28 17:51:14 +0400

    start golange web project - blog and add database phpadmin

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"web-service/database"
 
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	database.Connect()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("ERROR LOADING .end files")
+	}
+
+	port := os.Getenv("PORT")
+	app := fiber.New()
+	app.Listen(":" + port)
 }
